7d8c28fc53394dc5e0ce8573617fe25ec3e1e812
nishitpatel1292 <73834549+nishitpatel1292@users.noreply.github.com>
2023-03-16 15:08:18 +0530

    week 2

--- week_1/src/components/Title.js
+++ week_1/src/components/Title.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import '../Title.css'
 export default function Title(props) {
   return (
-    <div>
-        <h3>{props.data.id} {props.data.title}</h3>
-        <h3> {props.data.description}
+    <>
+        <h3 className='title'>{props.data.id} {props.data.title}</h3>
+        <h3 className='desc'> {props.data.description}
         </h3>
-    </div>
+    </>
   )
 }
