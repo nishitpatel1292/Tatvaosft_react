5842c3185889316d9ebd08e13594731c821f4c96
nishitpatel1292 <73834549+nishitpatel1292@users.noreply.github.com>
2023-03-16 14:47:34 +0530

    changed props

--- week_1/src/components/Title.js
+++ week_1/src/components/Title.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Title({data}) {
+export default function Title(props) {
   return (
     <div>
-        <h3>{data.id} {data.title}</h3>
-        <h3> {data.description}
+        <h3>{props.data.id} {props.data.title}</h3>
+        <h3> {props.data.description}
         </h3>
     </div>
   )
